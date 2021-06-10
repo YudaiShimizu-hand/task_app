3628b3fafb2c5adaceef029610af2561792f0a8a
Yudai Shimizu <y.s0922.1999@gmail.com>
2021-06-10 23:33:41 +0900

    タスクへのアクセス権限

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+      @tasks = Task.where(user_id: current_user.id) #ログインしているユーザーのTaskデータだけを読み出す
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,23 +12,26 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    @task = Task.new(task_params.merge(user_id: current_user.id)) #ログインしているユーザーのTaskデータの登録
+    if @task.save
+    redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render 'new'
+    end
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   def destroy
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password #passwordハッシュ化
+
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
+    has_many :tasks
 end
