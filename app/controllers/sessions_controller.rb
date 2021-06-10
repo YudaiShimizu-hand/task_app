c7ccc4bc8ea92975c254bcac4226feca80251d42
Yudai Shimizu <y.s0922.1999@gmail.com>
2021-06-10 15:16:26 +0900

    ユーザー登録,session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email]) #送られてきたメールアドレスでユーザーを検索する
+    if user&.authenticate(session_params[:password]) #ユーザーが見つかった場合パスワードによる認証をauthenticateメソッドを行う
+      session[:user_id] = user.id #認証に成功した場合セッションにuser_idを格納
+      redirect_to root_url, notice: 'ログインしました。'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    reset_session #session内の情報を全て消す
+    redirect_to root_url, notice: 'ログアウトしました。'
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    has_secure_password #passwordハッシュ化
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+end
