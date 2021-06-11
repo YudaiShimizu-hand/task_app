0a4a9eab0f05f67f07c9e1b31ef19152090150c9
Yudai Shimizu <y.s0922.1999@gmail.com>
2021-06-12 01:11:56 +0900

    Rspec

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ApplicationRecord
      belongs_to :user
 
-     scope :recent, -> { order(created_at) }
+     validates :name, presence: true
+     validates :description, presence: true
+
+     scope :recent, -> { order(created_at: :desc) }
      #繰り返し使用される絞り込み条件をスッキリ
 end
